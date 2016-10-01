ad6029958dfb43d91b93e742cc9027014a9af688
areeser <areeser@email.sc.edu>
2016-10-01 12:30:21 -0400

    Project Begin

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+    public float speed = 10.0f;
+    public float jumpForce = 10.0f;
+    public float bounceForce = 1000.0f;
+    public static bool touchingGround;
+    public static bool facingRight = true;
+    public Collision2D ColliInfo;
+    public Vector2 vect;
+	// Use this for initialization
+	void Start () {
+        touchingGround = true;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.RightArrow)) {
+            gameObject.transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
+            facingRight = true;
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow)) {
+            gameObject.transform.Translate(new Vector3(-speed * Time.deltaTime, 0, 0));
+            facingRight = false;
+        }
+        if (!(touchingGround)) {
+            OnCollisionEnter2D(ColliInfo);
+        }
+        else if (Input.GetKeyDown(KeyCode.Space)) {
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce), ForceMode2D.Force);
+            touchingGround = false;
+        }
+        vect = gameObject.GetComponent<Rigidbody2D>().velocity;
+    }
+    void OnCollisionEnter2D(Collision2D colliInfo) {
+        if (colliInfo.gameObject.tag == "Ground") {
+            touchingGround = true;
+        }
+        if (colliInfo.gameObject.tag == "Trampoline") {
+            touchingGround = false;
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, bounceForce), ForceMode2D.Force);
+        }
+    }
+}
---
+++ Assets/Scripts/Trampoline.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Trampoline : MonoBehaviour {
+    public float timer = 0.0f;
+    public float despawnTime = 2.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+        if (timer >= despawnTime) {
+            Destroy(gameObject);
+        }
+	}
+}
