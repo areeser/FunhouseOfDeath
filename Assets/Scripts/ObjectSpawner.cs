9ac2c7487c7093752ffbdb0a62e9da8b5fca1e01
areeser <areeser@email.sc.edu>
2016-10-26 22:06:02 -0400

    Created Balls of Death and Generic Spawner

---
+++ Assets/Scripts/ObjectSpawner.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectSpawner : MonoBehaviour {
+    public GameObject obj;
+    public Vector3 spawnPoint = new Vector3(0, 0, 0);
+    public float timer = 0.0f;
+    public float spawnFrequency = 1.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= spawnFrequency)
+        {
+            Instantiate(obj, gameObject.transform.position, new Quaternion());
+            timer = 0.0f;
+        }
+    }
+}
---
+++ Assets/Scripts/SoulOfDeath.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoulOfDeath : MonoBehaviour {
+    public Vector3 movement = new Vector3(0, 0, 0);
+    public float timer = 0;
+    public float despawnTime = 4.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        gameObject.transform.Translate(movement * Time.deltaTime);
+        timer += Time.deltaTime;
+        if (timer >= despawnTime) {
+            Destroy(gameObject);
+        }
+	}
+}
